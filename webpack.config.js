523e83e121312e7e56ab12c0d8d7f10ecac95e5b
GamorySensei <theo.gamory@metznumericschool.fr>
2023-12-05 12:01:45 +0100

    Génération des fichiers css dans un fichier séparé

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	entry: "./assets/js/main.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "public/js"),
+		path: path.resolve(__dirname, "public/build"),
 	},
 	module: {
 		rules: [
@@ -16,7 +17,7 @@ module.exports = {
 				test: /\.s[ac]ss$/i,
 				use: [
 					// Creates `style` nodes from JS strings
-					"style-loader",
+					MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
 					"css-loader",
 					// Compiles Sass to CSS
@@ -25,4 +26,12 @@ module.exports = {
 			},
 		],
 	},
+    plugins: [
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	]
 };
